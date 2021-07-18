ec8f5c7859f65aa31f03ddaf0f1b05aa62adf055
guoholehole <41961591+guoholehole@users.noreply.github.com>
2021-07-18 18:27:32 +0800

    第一次上传

---
+++ demo.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    let box = document.getElementsByClassName('box')[0];
+    box.addEventListener("click",()=>{
+        alert('这是测试代码');
+    })
+    // box.addEventListener('click',() => {
+    //     this.alert('这是测试代码');
+    // })
+    
+}
